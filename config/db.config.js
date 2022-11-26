3efe5ba8861521ea6b5e429f64c03bd2704c0c19
Malboyoo <malboyoo@gmail.com>
2022-11-26 10:42:32 +0100

    hide db config value

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  USER: "Malbo",
-  ADDRESS: "cluster0.xuzjnbm.mongodb.net",
-  DB: "hottakes",
-  PWD: "admin123",
+  USER: "*****",
+  ADDRESS: "********************",
+  DB: "***********",
+  PWD: "************",
 };
